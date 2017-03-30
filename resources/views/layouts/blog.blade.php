62cb54097dd9744db303ae4a58fd3a6b3b8e1acc
yopidax <y.y.yopidax07@gmail.com>
2017-03-30 23:24:57 +0900

    add blog views

---
+++ resources/views/layouts/blog.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+      <meta charset="UTF-8">
+      <title>Blog</title>
+  </head>
+  <body>
+      <div class="container">
+          <div class="row">
+              <h1>L5Blog</h1>
+              <div class="col-md-12">
+                  @yield('content')
+              </div>
+          </div>
+      </div>
+  </body>
+</html>
